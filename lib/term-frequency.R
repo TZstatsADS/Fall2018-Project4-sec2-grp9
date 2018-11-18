def817cdfe3a619fb45fd3e0b622471015b5e673
Levichasedream <42635712+Levichasedream@users.noreply.github.com>
2018-11-18 13:22:43 -0500

    Add files via upload

---
+++ lib/term-frequency.R
@@ -0,0 +1,72 @@
+
+library(tm)
+library(topicmodels)
+data('AssociatedPress', package = 'topicmodels')
+
+
+terms <- Terms(AssociatedPress) #extract the unqiue terms 
+head(terms)
+length(terms)
+
+library(dplyr)
+library(tidytext)
+ap_td <- tidy(AssociatedPress)
+ap_td  #the matrix: the document, the term, the count 
+tf <- ap_td %>% 
+  count(term, sort =F, wt=count) 
+head(tf, 20) # the frequency of the term 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
