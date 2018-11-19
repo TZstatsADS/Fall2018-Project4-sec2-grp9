bc523d2d772ed9ce011b2ad35a3426acf6a878dc
Levichasedream <42635712+Levichasedream@users.noreply.github.com>
2018-11-18 23:44:29 -0500

    Add files via upload

---
+++ lib/delete.R
@@ -0,0 +1,79 @@
+
+
+#deletion
+delete <- function(error, letter, i){
+  return(paste(substring(error, c(0, i), c(i-1,nchar(error)+1)), collapse=letter))
+
+}
+delete('error', 'j', 1)
+final_delete <- function(error){
+  word_list <- list()
+  for (i in 1 : (nchar(error)+1)){
+    ith_list <- sapply(letters, delete, error=error, i=i)
+    word_list[[i]] <- ith_list
+  }
+  words <- data_frame(unname(unlist(word_list)))
+  colnames(words) <- 'words' # colname is 'word'
+ return(words)
+  }
+final_delete('error')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
